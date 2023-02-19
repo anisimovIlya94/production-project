210379a46a13c9ccbbfc4c69ec123f7ebd63f8a3
Anisimov Ilya <anisimov_ilya26@mail.ru>
2023-02-19 19:25:40 +0500

    add github actions

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -15,12 +15,6 @@ export default ({ config }: { config: webpack.Configuration }) => {
     config.resolve?.modules?.push(paths.src)
     config.resolve?.extensions?.push(".ts", ".tsx")
 
-    // config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
-    //     if (/svg/.test(rule.test as string)) {
-    //         return {...rule, exclude: /\.svg$/}
-    //     }
-    // })
-
     if (config.module?.rules) {
         config.module.rules = config.module?.rules?.map((rule: webpack.RuleSetRule | "...") => {
         if (rule !== "..." && /svg/.test(rule.test as string)) {
